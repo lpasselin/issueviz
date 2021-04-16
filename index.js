65bf9de3416d6de8d352551c09c2f188ac62f5b1
Louis-Philippe Asselin <louisphilippeasselin@gmail.com>
2021-04-16 12:43:41 -0400

    working first iteration

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
 let issueMap;
 let stakeholderMap;
 
+let width = 400;
+let height = 400;
+let format = d3.format(",d");
+let color = d3.scaleLinear()
+    .domain([0, 5])
+    .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+    .interpolate(d3.interpolateHcl);
+
 (async () => {
     // main
-    Papa.parse("/issues_matrix_small.csv", {
+    await Papa.parse("/issues_matrix_small.csv", {
         download: true,
-        complete: processCsvDataToGlobals,
+        complete: processCsvDataToGlobalsAndPlot,
         header: true,
     });
 })()
 
-function processCsvDataToGlobals(r) {
+
+function processCsvDataToGlobalsAndPlot(r) {
     if (r.errors.length) {
         console.log("error obtaining csv data or parsing.");
         console.log(r.errors);
@@ -65,10 +74,11 @@ function processCsvDataToGlobals(r) {
         issueMap.set(issue["#Issue"], node);
         parent.children.push(issueMap.get(issue["#Issue"]));
     }
+    chart();
 }
 
 function simplifyIssueMapForD3(theMap) {
-    let theMapNames = [...theMap.keys()];
+    let theMapNames = Array.from(theMap.keys());
     let dataNode = {
         name: theMapNames[0],
         children: []
@@ -91,112 +101,106 @@ function simplifyIssueMapForD3(theMap) {
     return dataNode;
 }
 
+function pack(data) {
+    return d3.pack()
+    .size([width, height])
+    .padding(3)
+    (d3.hierarchy(data)
+        .sum(d => d.value)
+        .sort((a, b) => b.value - a.value));
+}
 
-//////////////////////////
-// chart = {
-//     const root = pack(data);
-//     let focus = root;
-//     let view;
+function chart() {
+    // TODO split init and drawing code. To enable drawing from somewhere else.
+    // currently, it removes the old div and creates a new one
+    d3.select("#chart").remove();
+    const div = d3.select("body")
+        .append('div')
+        .attr("id", "chart")
+        .style("background-color", "red")
+        .style("max-width", "90vh");
 
-//     var myData = [
-//         { name: 'a', value: 1 },
-//         { name: 'b', value: 2 },
-//     ]
+    const root = pack(simplifyIssueMapForD3(issueMap));
+    let focus = root;
+    let view;
+
+    var myData = [
+        { name: 'a', value: 1 },
+        { name: 'b', value: 2 },
+    ]
     
-//     const div = d3.create("div")
-//         .style("background-color", "red")
-//         .style("width", "100%")
-    
-//     var select = div.append("div")
-//         .style("display", "block")
-//         .append("select")
-//         .attr("id", "some_id")
-//         .attr("onchange", d => d);
+    var select = div.append("div")
+        .style("display", "block")
+        .append("select")
+        .attr("id", "some_id")
+        .attr("onchange", d => d);
 
-//     select.selectAll("option")
-//         .data(myData).enter()
-//         .append("option")
-//         .html(function (d) {
-//             return d.name;
-//         });
+    select.selectAll("option")
+        .data(myData).enter()
+        .append("option")
+        .html(function (d) {
+            return d.name;
+        });
 
-//     const svg = div.append("svg").style("display", "block")
-//         .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-//         .style("display", "block")
-//         .style("margin", "0 -14px")
-//         .style("background", color(0))
-//         .style("cursor", "pointer")
-//         .on("click", (event) => zoom(event, root));
+    const svg = div.append("svg").style("display", "block")
+        .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+        .style("display", "block")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .style("margin", "0 -14px")
+        .style("background", color(0))
+        .style("cursor", "pointer")
+        .on("click", (event) => zoom(event, root));
 
-//     const node = svg.append("g")
-//         .selectAll("circle")
-//         .data(root.descendants().slice(1))
-//         .join("circle")
-//         .attr("fill", d => d.children ? color(d.depth) : "white")
-//         .attr("pointer-events", d => !d.children ? "none" : null)
-//         .on("mouseover", function () { d3.select(this).attr("stroke", "#000"); })
-//         .on("mouseout", function () { d3.select(this).attr("stroke", null); })
-//         .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
+    const node = svg.append("g")
+        .selectAll("circle")
+        .data(root.descendants().slice(1))
+        .join("circle")
+        .attr("fill", d => d.children ? color(d.depth) : "white")
+        .attr("pointer-events", d => !d.children ? "none" : null)
+        .on("mouseover", function () { d3.select(this).attr("stroke", "#000"); })
+        .on("mouseout", function () { d3.select(this).attr("stroke", null); })
+        .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
 
-//     const label = svg.append("g")
-//         .style("font", "10px sans-serif")
-//         .attr("pointer-events", "none")
-//         .attr("text-anchor", "middle")
-//         .selectAll("text")
-//         .data(root.descendants())
-//         .join("text")
-//         .style("fill-opacity", d => d.parent === root ? 1 : 0)
-//         .style("display", d => d.parent === root ? "inline" : "none")
-//         .text(d => d.data.name);
+    const label = svg.append("g")
+        .style("font", "10px sans-serif")
+        .attr("pointer-events", "none")
+        .attr("text-anchor", "middle")
+        .selectAll("text")
+        .data(root.descendants())
+        .join("text")
+        .style("fill-opacity", d => d.parent === root ? 1 : 0)
+        .style("display", d => d.parent === root ? "inline" : "none")
+        .text(d => d.data.name);
 
-//     zoomTo([root.x, root.y, root.r * 2]);
+    zoomTo([root.x, root.y, root.r * 2]);
 
-//     function zoomTo(v) {
-//     const k = width / v[2];
+    function zoomTo(v) {
+        const k = width / v[2];
 
-//     view = v;
+        view = v;
 
-//     label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-//     node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-//     node.attr("r", d => d.r * k);
-// }
+        label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+        node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+        node.attr("r", d => d.r * k);
+    }
 
-// function zoom(event, d) {
-//     const focus0 = focus;
+    function zoom(event, d) {
+        const focus0 = focus;
 
-//     focus = d;
+        focus = d;
 
-//     const transition = svg.transition()
-//         .duration(event.altKey ? 7500 : 750)
-//         .tween("zoom", d => {
-//             const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-//             return t => zoomTo(i(t));
-//         });
+        const transition = svg.transition()
+            .duration(event.altKey ? 7500 : 750)
+            .tween("zoom", d => {
+                const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+                return t => zoomTo(i(t));
+            });
 
-//     label
-//         .filter(function (d) { return d.parent === focus || this.style.display === "inline"; })
-//         .transition(transition)
-//         .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-//         .on("start", function (d) { if (d.parent === focus) this.style.display = "inline"; })
-//         .on("end", function (d) { if (d.parent !== focus) this.style.display = "none"; });
-// }
-// return div.node();
-//   }
-
-
-// pack = data => d3.pack()
-//     .size([width, height])
-//     .padding(3)
-//     (d3.hierarchy(simplifyIssueMapForD3(issueMap))
-//         //(d3.hierarchy(data)
-//         .sum(d => d.value)
-//         .sort((a, b) => b.value - a.value));
-
-// width = 932;
-// height = width;
-// format = d3.format(",d");
-// color = d3.scaleLinear()
-//     .domain([0, 5])
-//     .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-//     .interpolate(d3.interpolateHcl);
-// d3 = require("d3@6")
+        label
+            .filter(function (d) { return d.parent === focus || this.style.display === "inline"; })
+            .transition(transition)
+            .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+            .on("start", function (d) { if (d.parent === focus) this.style.display = "inline"; })
+            .on("end", function (d) { if (d.parent !== focus) this.style.display = "none"; });
+    }
+}
